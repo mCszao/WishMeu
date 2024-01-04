f4bdc98d9d0d68e7077794ad6f0e6c544659c580
Marcos Vinicius <marc.khk11@gmail.com>
2024-01-04 00:06:46 -0400

    feat: new class WishList and his own DAO

---
+++ model/List.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace model;
+
+class WishList {
+    private $id;
+    private $name;
+    private $desc;
+
+    public function __constructor($id, $name, $desc){
+        $this->id = $id;
+        $this->name = $name;
+        $this->desc = $desc;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getDesc(){
+        return $this->desc;
+    }
+
+    public function setName($name){
+        $this->name = $name;
+    }
+
+    public function setDesc($description){
+        $this->desc = $description;
+    }
+}
+
+interface WishListDAO {
+    public function save(WishList $list);
+    public function findAll();
+    public function findByName($string);
+}
