ce26dd032d8bbb03cf4cf5a2a33b618968683623
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-03 01:11:58 -0400

    refactor: introduce mvc

---
+++ src/Config.php
@@ -0,0 +1,13 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = '/wishmeu/public';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = 'localhost';
+    const DB_DATABASE = 'whishmeu';
+    const DB_USER = 'root';
+    const DB_PASSWORD = '*Mcs6468';
+    const DB_PORT = 3010;
+}
