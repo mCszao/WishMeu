b544142aaf5e2ee84915f92bc5203cff2120a5bf
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-04 02:18:11 -0400

    feat: new routes to render add list e save process

--- src/routes.php
+++ src/routes.php
@@ -4,3 +4,5 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
+$router->get('/list/add', 'WishListController@add');
+$router->post('/list/save', 'WishListController@save');
