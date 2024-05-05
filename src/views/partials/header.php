1f3147ed78ee45cdd2572f9eb5bcfd2738a0e4fe
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-05 17:55:27 -0400

    feat: new font style

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$title?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body style="display: flex; align-items: center; flex-direction: column">
+<body style="font-family: Arial; display: flex; align-items: center; flex-direction: column">
     <header>
         <h1>WishMeu</h1>
     </header>
