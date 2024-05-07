7ba23f82471b8b121f368a1ce4652cb4db9a8f12
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-06 22:38:21 -0400

    feat: menu partial to every screen

---
+++ src/views/partials/menu.php
@@ -0,0 +1,24 @@
+<style>
+    .menu-container {
+        background-color: #040F0F;
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        border-radius: 1rem;
+        position: fixed;
+        top: 50%;
+        left: 2%;
+        color: white;
+        gap: 1rem;
+    }
+
+    .menu-item {
+        
+    }
+
+</style>
+<div class="menu-container">
+    <a class="menu-item" href="<?=$base.'/list/add'?>"><i>+ WishList</i></a>
+    <a class="menu-item" href="<?=$base.'/categorie/add'?>"><i>+ Categorias</i></a>
+    <a class="menu-item" href="<?=$base.'/item/add'?>"><i>+ Items</i></a>
+</div>  
