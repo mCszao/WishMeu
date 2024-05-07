f7c8cbf94ff6c8fef91f2c06208c762b0fd4ef83
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-06 22:39:05 -0400

    fix: alter view add wishlist to render partial baseForm

--- src/views/pages/addWishList.php
+++ src/views/pages/addWishList.php
@@ -1,17 +1,6 @@
 <?php $render('header', ['title' => 'Nova Wish List, Meu']); ?>
 
-<h2>Nova Lista</h2>
+<h2>Lista</h2>
 
-<form style=" display: flex; gap: 0.5rem; flex-direction: column" method="POST" action="<?=$base;?>/list/save">
-    <div>
-        <label for="name">Nome da Lista:</label>
-        <input type="text" name="name" id="name" />
-    </div>
-    <div>
-        <label for="desc">Descrição da Lista:</label>
-        <input type="text" name="desc" id="desc"/>
-    </div>
-    <input type="submit" value="Adicionar">
-</form>
+<?php $render('baseForm', ['keyword' => 'list', 'keywordPT' => 'Lista'])?>
 
-<?php $render('footer');?>
