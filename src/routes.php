e813def9d3e5d8f101fea2d5a00cb9a2816ddb64
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-06 22:39:53 -0400

    feat: add categorie and item routes

--- src/routes.php
+++ src/routes.php
@@ -6,3 +6,7 @@ $router = new Router();
 $router->get('/', 'HomeController@index');
 $router->get('/list/add', 'WishListController@add');
 $router->post('/list/save', 'WishListController@save');
+$router->get('/categorie/add', 'CategorieController@add');
+$router->post('/categorie/save', 'CategorieController@save');
+$router->get('/item/add', 'ItemController@add');
+$router->post('/item/save', 'ItemController@save');
