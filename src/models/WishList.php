76b7b6c6bf85c712faca92c53cd495a54fbb70ff
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-08 04:23:00 -0400

    fix; select was no usable

--- src/models/WishList.php
+++ src/models/WishList.php
@@ -13,6 +13,6 @@ class WishList extends Model {
     }
 
     public static function loadInfoList($id){
-        return WishList::select(['wishlists.name', 'wishlists.description', 'i.name as item_name', 'i.observations', 'itl.min_value', 'itl.max_value', 'itl.payed_value', 'itl.conclued'])->innerJoin('itemtolists as itl', 'itl.id_list', '=', 'wishlists.id')->join('items as i', 'i.id', '=', 'itl.id_item')->where('wishlists.id', $id)->get();
+        return WishList::select(['wishlists.id','wishlists.name','itl.id as id_item_lista','i.name as item_name', 'i.observations','c.name as categorie_name', 'itl.min_value', 'itl.max_value', 'itl.payed_value', 'itl.conclued'])->innerJoin('itemtolists as itl', 'itl.id_list', '=', 'wishlists.id')->join('items as i', 'i.id', '=', 'itl.id_item')->join('categories as c', 'c.id', '=', 'i.category_id')->where('wishlists.id', $id)->execute();
     }
 }
