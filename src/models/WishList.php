6338c05b382f4c7eca69fb41fd416c60c564efea
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-08 02:41:56 -0400

    fix: did not diff name item from name list

--- src/models/WishList.php
+++ src/models/WishList.php
@@ -5,7 +5,7 @@ namespace src\models;
 use \core\Model;
 class WishList extends Model {
     public static function loadCompleteLists(){
-        return WishList::select(['wishlists.name', 'wishlists.description', 'i.name', 'i.observations', 'itl.min_value', 'itl.max_value', 'itl.payed_value', 'itl.conclued'])->innerJoin('itemtolists as itl', 'itl.id_list', '=', 'wishlists.id')->join('items as i', 'i.id', '=', 'itl.id_item')->get();
+        return WishList::select(['wishlists.name', 'wishlists.description', 'i.name as item_name', 'i.observations', 'itl.min_value', 'itl.max_value', 'itl.payed_value', 'itl.conclued'])->innerJoin('itemtolists as itl', 'itl.id_list', '=', 'wishlists.id')->join('items as i', 'i.id', '=', 'itl.id_item')->get();
     }
 
     public static function loadResumeLists(){
