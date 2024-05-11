89f985108ea57114832a74c9d939be73762c95d5
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-11 04:41:05 -0400

    fix: style conclued field and categorie value after saving

--- public/js/dialog.js
+++ public/js/dialog.js
@@ -10,7 +10,7 @@ function closeModal(){
     dialog.close();
 };  
 
-async function addItem(idList,itemId, itemName, itemObs, path){
+async function addItem(idList,itemId, itemName, itemObs, itemCat, path){
     const tableBody = document.querySelector('.body');
     const min = document.getElementById('min'+itemId).value;
     const max = document.getElementById('max'+itemId).value;
@@ -25,8 +25,8 @@ async function addItem(idList,itemId, itemName, itemObs, path){
     <tr class="table__content__row" id="${itemId}">
         <td class="table__content__row__data">${itemName}</td>
         <td class="table__content__row__data">${itemObs}</td>
-        <td class="table__content__row__data">${itemObs}</td>
-        <td class="table__content__row__data">0</td>
+        <td class="table__content__row__data">${itemCat}</td>
+        <td class="table__content__row__data">'❌'</td>
         <td class="table__content__row__data">R$${min}</td>
         <td class="table__content__row__data">R$${max}</td>
         <td class="table__content__row__data">R$0</td>
