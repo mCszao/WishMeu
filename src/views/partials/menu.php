ce5fd1445a9c2ae3102d379cfee59839d1d04d69
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-10 21:54:25 -0400

    fix: change menu position

--- src/views/partials/menu.php
+++ src/views/partials/menu.php
@@ -1,20 +1,38 @@
 <style>
     .menu-container {
-        background-color: #040F0F;
+        background-color:#034159;
+        box-shadow: 1px 3px 15px 2px #000000;
         display: flex;
         flex-direction: column;
         padding: 1rem;
         border-radius: 1rem;
+        height: 0.1rem;
         position: fixed;
-        top: 50%;
+        top: 1%;
         left: 1%;
         color: white;
         gap: 1rem;
     }
 
-    .menu-item {
-        color: #EE6055;
+    
+    .menu-container:hover{
+        box-shadow: 1px 3px 15px 2px #fff;
+        height: auto;
+        transition: ease-in .8s;
     }
+    .menu-container:hover > .menu-item {
+        opacity: 1;
+        transition: ease-out 1s;
+    }
+
+    .menu-item {
+        opacity: 0;
+        color: #02735E;
+        
+        
+    }
+
+   
 
 </style>
 <div class="menu-container">
