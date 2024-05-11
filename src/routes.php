0a27225e04e7ed6eca57685a057fdf9b4fb9f70a
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-11 03:09:14 -0400

    feat: new editr route

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,9 @@ $router = new Router();
 
 $router->get('/', 'HomeController@index');
 $router->get('/list/add', 'WishListController@add');
+$router->get('/list/add/{id}', 'WishListController@add');
 $router->post('/list/save', 'WishListController@save');
+$router->post('/list/edit/{id}', 'WishListController@edit');
 $router->get('/list/{id}', 'WishListController@details');
 $router->get('/categorie/add', 'CategorieController@add');
 $router->post('/categorie/save', 'CategorieController@save');
