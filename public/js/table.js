78ed4ac484cdd6c6253fdd743810e8b86575b539
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-17 03:21:51 -0400

    feat: new function to calculate totals after edit

--- public/js/table.js
+++ public/js/table.js
@@ -5,11 +5,13 @@ async function editRow(correlationId){
 
     if(minNode.querySelector('input') == null) {
         this.event.currentTarget.innerHTML = '<img src="https://icons.iconarchive.com/icons/arturo-wibawa/akar/128/save-icon.png" width="24" height="24">';
-    
-    
-        minNode.innerHTML = `<input type="number" id="inputMin${correlationId}" value='${minNode.innerHTML.slice(2)}'/>`;
-        maxNode.innerHTML = `<input type="number" id="inputMax${correlationId}" value='${maxNode.innerHTML.slice(2)}'/>`;
-        payedNode.innerHTML = `<input type="number" id="inputPayed${correlationId}" value='${payedNode.innerHTML.slice(2)}'/>`;
+        let minIptValue = Number(minNode.innerHTML.slice(2));
+        let maxIptValue = Number(maxNode.innerHTML.slice(2));
+        let payedIptValue = Number(payedNode.innerHTML.slice(2));
+        calculateTotals(minIptValue,maxIptValue, payedIptValue, "-");
+        minNode.innerHTML = `<input type="number" id="inputMin${correlationId}" value='${minIptValue}'/>`;
+        maxNode.innerHTML = `<input type="number" id="inputMax${correlationId}" value='${maxIptValue}'/>`;
+        payedNode.innerHTML = `<input type="number" id="inputPayed${correlationId}" value='${payedIptValue}'/>`;
     }else {
         let minValue = Number(document.getElementById(`inputMin${correlationId}`).value);
         let maxValue = Number(document.getElementById(`inputMax${correlationId}`).value);
@@ -37,4 +39,32 @@ async function resetRow(minValue, maxValue, min, max, payed, correlationId){
     min.innerHTML = 'R$'+minValue;
     max.innerHTML = 'R$'+maxValue;
     payed.innerHTML = 'R$'+payedValue;
+    calculateTotals(minValue,maxValue, payedValue, "+");
+}
+
+function calculateTotals(minItem, maxItem, payedItem, operator) {
+    if(operator == "" || operator == null) {
+        return;
+    }
+    let totMin = Number(document.getElementById('totalMin').innerHTML);
+    let totMax = Number(document.getElementById('totalMax').innerHTML);
+    let totPayed = Number(document.getElementById('totalPayed').innerHTML);
+    verifyIsO([minItem, maxItem, payedItem]);
+    switch (operator) {
+        case '+':
+            document.getElementById('totalMin').innerHTML = totMin + minItem;
+            document.getElementById('totalMax').innerHTML = totMax + maxItem;
+            document.getElementById('totalPayed').innerHTML = Number(totPayed) + Number(payedItem);
+            break;
+        case '-':
+            document.getElementById('totalMin').innerHTML = totMin - minItem;
+            document.getElementById('totalMax').innerHTML = totMax - maxItem;
+            document.getElementById('totalPayed').innerHTML = totPayed - payedItem;
+            break;
+    }
+
+}
+
+function verifyIsO(list){
+    Array.from(list).forEach(num => num = num == 0 ? 1 : num);
 }
