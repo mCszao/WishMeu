548e60837d71c22c89521bee41c2f7b580129f8b
Marcos Vinicius <marc.khk11@gmail.com>
2024-05-19 03:56:05 -0400

    refact: moving correlation functions item list into a only especified class

--- src/controllers/ItemController.php
+++ src/controllers/ItemController.php
@@ -40,34 +40,4 @@ class ItemController extends Controller {
     
     }
 
-    public function itemToList(){
-        $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-        
-        ItemToList::insert([
-            'id_list' => $data['idList'],
-            'id_item' => $data['itemId'],
-            'max_value' => $data['max'],
-            'min_value' => $data['min']
-        ])->execute();
-        
-
-    }
-
-    public function delete($args){
-        $id = $args['id'];
-        $idList = ItemToList::select('id_list')->where('id', $id)->one();
-        ItemToList::delete()->where('id', $id)->execute();
-        
-        $this->redirect('/list/'.$idList['id_list']);
-    }
-
-    public function edit($args){
-        $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-        
-        ItemToList::update(['min_value' => $data['min'],
-                            'max_value' => $data['max'],
-                            'payed_value' => $data['payed']]
-        )->where('id', $args['id'])->execute();
-        
-    }
 }
---
+++ src/controllers/ItemToListController.php
@@ -0,0 +1,38 @@
+<?php 
+namespace src\controllers;
+
+use \core\Controller;
+use \src\models\ItemToList;
+
+class ItemToListController extends Controller {
+    public function addItemToList(){
+        $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        
+        ItemToList::insert([
+            'id_list' => $data['idList'],
+            'id_item' => $data['itemId'],
+            'max_value' => $data['max'],
+            'min_value' => $data['min']
+        ])->execute();
+        
+
+    }
+
+    public function delete($args){
+        $id = $args['id'];
+        $idList = ItemToList::select('id_list')->where('id', $id)->one();
+        ItemToList::delete()->where('id', $id)->execute();
+        
+        $this->redirect('/list/'.$idList['id_list']);
+    }
+
+    public function edit($args){
+        $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        
+        ItemToList::update(['min_value' => $data['min'],
+                            'max_value' => $data['max'],
+                            'payed_value' => $data['payed']]
+        )->where('id', $args['id'])->execute();
+        
+    }
+}
